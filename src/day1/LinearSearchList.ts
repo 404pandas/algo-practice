ef741bcc066c3410e6c60b070cc397de76677464
404pandas <mary.panda.jackson@gmail.com>
2025-06-13 14:13:45 -0400

    Linear search done

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,14 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let index = 0; index < haystack.length; index++) {
+        if (haystack[index] === needle) {
+            return true;
+        }
+    }
+    return false;
 }
+
+// linear_search([1, 2, 3, 4, 5], 3); // true
+// linear_search([1, 2, 3, 4, 5], 6); // false
